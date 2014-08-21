e79375da621c31bf579a38ddaed9197e35cd5f04
Daniel Rahier <daniel@rahier.biz>
2014-08-21 17:47:31 +0200

    Refactored Viewport Pane into its own class

--- src/Viewport.cpp
+++ src/Viewport.cpp
@@ -2,39 +2,12 @@
 
 #include "ViewportTitleBar.h"
 
-#include <QFileInfo>
-#include <QLabel>
-#include <QHBoxLayout>
-#include <QVBoxLayout>
 
 
-Viewport::Viewport(const int viewport_number, QWidget *parent)
-	: QWidget(parent)
-	, m_draw_area(new QLabel("foo", this))
-	, m_title_bar(new ViewportTitleBar(viewport_number))
+#include <vector>
+
+
+void Viewport::resizeEvent(QResizeEvent* event)
 {
-	QVBoxLayout* main_layout = new QVBoxLayout;
-	main_layout->setSpacing(0);
-
-	main_layout->addWidget(m_title_bar);
-	
-	connect(m_title_bar, SIGNAL(close_button_clicked()), this, SIGNAL(viewport_closed()));
-	connect(m_title_bar, SIGNAL(scene_changed(const QFileInfo&)), this, SLOT(load_scene(const QFileInfo&)));
-
-	QHBoxLayout* layout = new QHBoxLayout;
-	layout->addWidget(m_draw_area);
-	layout->setMargin(0);
-	layout->setSpacing(0);
-
-	main_layout->addLayout(layout);
-	setLayout(main_layout);
-
-	m_draw_area->setFrameStyle(QFrame::Box);
-	m_draw_area->setLineWidth(3);
-	m_draw_area->setMinimumSize(100, 100);
-}
-
-void Viewport::load_scene(const QFileInfo& file_info)
-{
-	m_draw_area->setText(file_info.baseName());
+	emit resized();
 }
--- src/Viewport.h
+++ src/Viewport.h
@@ -1,30 +1,24 @@
 #pragma once
 
-#include <QWidget>
+#include <QLabel>
 
 
-class QFileInfo;
-class QLabel;
-class QScrollArea;
-class QGroupBox;
-class ViewportTitleBar;
-
-
-class Viewport : public QWidget
+class Viewport : public QLabel
 {
 	Q_OBJECT
-public:
-	explicit Viewport(const int viewport_number, QWidget* parent = nullptr);
 
-	//virtual QSize sizeHint() const { return QSize(100, 100); }
+public:
+	explicit Viewport(const QString text, QWidget* parent)
+		: QLabel(text, parent)
+	{
+	}
 
 signals:
-	void viewport_closed();
+	void resized();
 
-private slots:
-	void load_scene(const QFileInfo& file_info);
-
-private:
-	QLabel* m_draw_area;
-	ViewportTitleBar* m_title_bar;
+protected:
+	virtual void resizeEvent(QResizeEvent* event)
+	{
+		emit resized();
+	}
 };
